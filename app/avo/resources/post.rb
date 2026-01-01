06ec88b414237e279eb05aaf15822257fc3ba6da
Mason <bigmasonwang@gmail.com>
2026-01-01 22:58:44 +1100

    Fix Avo post resource date_time field type

--- app/avo/resources/post.rb
+++ app/avo/resources/post.rb
@@ -4,6 +4,6 @@ class Avo::Resources::Post < Avo::BaseResource
     field :title, as: :text
     field :body, as: :markdown
     field :slug, as: :text, readonly: true
-    field :published_at, as: :datetime
+    field :published_at, as: :date_time
   end
 end
